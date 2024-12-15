9e808e413fa7c32242a40b3436e81eb33b5d0a29
7899974376 <143157014+7899974376@users.noreply.github.com>
2024-12-15 23:41:45 +0530

    largest of three number

---
+++ largestfo3.java
@@ -0,0 +1,21 @@
+package full;
+
+public class largestfo3 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=10;
+        int c=30;
+
+        if(a>=b && a>=c){
+            System.out.println("a is largest");
+        }
+        else if(b>=a && b>=c){
+            System.out.println("b is largest");
+        }
+        else{
+            System.out.println("c is largest");
+        }
+
+    }
+    
+}
