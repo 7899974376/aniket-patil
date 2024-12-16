3a23ec673ca1197b6eca8e1685de7cf16dacd239
7899974376 <143157014+7899974376@users.noreply.github.com>
2024-12-17 00:00:46 +0530

    for loop

---
+++ forloop1.java
@@ -0,0 +1,50 @@
+package full;
+
+public class forloop1 {
+    public static void main(String[] args) {
+        /*//1 2 3 4 5 6 7 8 9 10
+        for(int i=1; i<=10; i++){
+            System.out.println(i);
+        }*/
+        /*// 146 147 148 149 150
+        for(int i=146; i<=150; i++){
+            System.out.println(i);
+        }*/
+
+        /*//10 9 8 7 6 5 4 3 2 1 
+        for(int i=10; i>=1; i--){
+            System.out.println(i);
+        }*/
+
+        /*//2 4 6 8 10 12 14 16 18 20
+        for(int i=2; i<=20; i=i+2){
+            System.out.println(i);
+        }*/
+
+        /*//3 6 9 12 15 18
+        for(int i=3; i<=20; i=i+3){
+            System.err.println(i);
+        }*/
+
+        /*//0 10 20 30 40 50 60 80 100
+        for(int i=0; i<=100; i+=10){
+            System.err.println(i);
+        }*/
+
+        /*// 0 20 40 60 80 100
+        for(int i=0; i<=100; i+=20){
+            System.err.println(i);
+        }*/
+
+        /*//odd number 1 3 5 7 9 
+        for(int i=1; i<=10; i+=2){
+            System.err.println(i);
+        }*/
+
+        //print the odd number 1 to 100;
+        for(int i=1; i<=100; i+=2){
+            System.err.println(i);
+        }
+    }
+    
+}
