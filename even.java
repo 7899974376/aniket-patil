6e8b26245d72ff179c63065a378a500f3030bd5c
7899974376 <143157014+7899974376@users.noreply.github.com>
2024-12-17 00:00:29 +0530

    even number

--- even.java
+++ even.java
@@ -1,17 +1,17 @@
-import java.util.Scanner;
+package full;
 
 public class even {
     public static void main(String[] args) {
-        int a;
-        Scanner sc=new Scanner(System.in);
-
-        System.out.println("enter the number");
-        a=sc.nextInt();
-
-        if(a%2==0)
-            System.out.print("even");
-        else
-        System.out.print("odd");
+        //using the for 
+        for(int i=0; i<=20; i+=2){
+            System.err.println(i);
+        }
+        /* using the for and if 
+        for(int i=0; i<=10; i++){
+            if(i%2==0){
+                System.err.println(i);
+            }
+        }*/
     }
     
 }
