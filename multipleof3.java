e88811d7dc437c606afa8db3036755a87bd1d9df
7899974376 <143157014+7899974376@users.noreply.github.com>
2024-12-17 00:06:57 +0530

    multiple of 3

---
+++ multipleof3.java
@@ -0,0 +1,13 @@
+package full;
+
+public class multipleof3 {
+    public static void main(String[] args) {
+        
+        for(int i=1;i<=15; i++){
+            if(i%3==0){
+            System.err.println(i);
+            }
+        }
+    }
+    
+}
